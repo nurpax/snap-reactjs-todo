d21f83caf1eb4a0efbcde8c9b5ee5bd3d3361033
Janne Hellsten <jjhellst@gmail.com>
2016-12-01 22:30:10 +0200

    delete dead code

--- src/Util.hs
+++ src/Util.hs
@@ -3,28 +3,17 @@
 
 module Util (
     reqJSON
-  , runHttpErrorExceptT
-  , hoistHttpError
   , writeJSON
   , jsonResponse
   , finishEarly
   ) where
 
-import           Control.Monad.Except
 import qualified Data.Aeson as A
 import qualified Data.ByteString as B
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as T
 
 import           Snap.Core
-import           Snap.Snaplet
 import qualified Snap.Snaplet.SqliteSimple.JwtAuth as J
 import           Snap.Snaplet.SqliteSimple.JwtAuth (jsonResponse, reqJSON)
-import           Application
-
-type H = Handler App App
-
-data HttpError = HttpError Int String
 
 -------------------------------------------------------------------------------
 -- | Set MIME to 'application/json' and write given object into
@@ -40,22 +29,3 @@ finishEarly code str = do
   modifyResponse $ addHeader "Content-Type" "text/plain"
   writeBS str
   getResponse >>= finishWith
-
-runHttpErrorExceptT :: ExceptT HttpError H (H ()) -> H ()
-runHttpErrorExceptT e = runExceptT e >>= either err id
-  where
-    err (HttpError errCode msg) = do
-      let m = T.encodeUtf8 . T.pack $ msg
-      logError m
-      finishEarly errCode m
-
-badReq :: String -> HttpError
-badReq msg = HttpError 400 msg
-
--- | Upgrade an 'Either' to an 'ExceptT'
-hoistEither :: Monad m => Either e a -> ExceptT e m a
-hoistEither = ExceptT . return
-
-hoistHttpError :: Monad m => Either String a -> ExceptT HttpError m a
-hoistHttpError (Left m)  = hoistEither . Left . badReq $ m
-hoistHttpError (Right v) = hoistEither . Right $ v
